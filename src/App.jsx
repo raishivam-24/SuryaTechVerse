e0f31b4c45e2efcb9fe1dc453f9bb5dc874dc29e
raishivam-24 <shivamrai1716@gmail.com>
2026-09-03 17:52:55 +0530

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
         <Services />
         <Work />
         <About />
-        <Team />
+        {/* <Team /> */}
         <Contact />
       </main>
       <Footer />
